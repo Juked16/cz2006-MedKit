5cb99e7c1a8cd117d98c91baee2b5ba5fb402826
Liu-Aiqi <72330141+Liu-Aiqi@users.noreply.github.com>
2022-03-30 23:36:01 +0800

    Add files via upload

--- cz2006MedKit/source/control/ForumMgr.java
+++ cz2006MedKit/source/control/ForumMgr.java
@@ -1,25 +1,41 @@
 package control;
+import boundary.ForumUI;
 import entity.*;
 
 public class ForumMgr {
 
 	/**
-	 * 
+	 * Add post to postlist if logged in and display error message otherwise
 	 * @param post
 	 */
 	public boolean addPost(Post post) {
 		// TODO - implement ForumMgr.addPost
-		throw new UnsupportedOperationException();
+		ForumUI forumInstance = new ForumUI();
+		AccountMgr accountmgr = new AccountMgr();
+		if (accountmgr.isAccountVerified(post.getUser()) == 0) {
+			forumInstance.displayErrorMessage();
+			return false;
+		}
+		Postlist postlist = new Postlist();
+		postlist.addPost(post);
+		return true;
 	}
 
 	/**
-	 * 
+	 * Add comment to post if logged in and display error message otherwise
 	 * @param post
 	 * @param comment
 	 */
 	public boolean addComment(Post post, Text comment) {
 		// TODO - implement ForumMgr.addComment
-		throw new UnsupportedOperationException();
+		ForumUI forumInstance = new ForumUI();
+		AccountMgr accountmgr = new AccountMgr();
+		if (accountmgr.isAccountVerified(comment.getUser()) == 0) {
+			forumInstance.displayErrorMessage();
+			return false;
+		}
+		post.addComment(comment);
+		return true;
 	}
 
 }
